50794136a79e502141720bad11e72fda1aab90bb
Robin K <dev.robinkurian@gmail.com>
2024-10-21 10:19:52 +0530

    fix: typo in linkedin url

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -52,7 +52,7 @@ export default function Home() {
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="hhttps://www.linkedin.com/in/developer-robin-kurian/"
+          href="https://www.linkedin.com/in/developer-robin-kurian/"
           target="_blank"
           rel="noopener noreferrer"
         >
