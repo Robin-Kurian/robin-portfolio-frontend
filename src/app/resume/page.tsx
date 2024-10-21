3b49dfad2034264f79bd5134a10c534f386375a7
Robin K <dev.robinkurian@gmail.com>
2024-10-21 10:32:55 +0530

    feat: resume section

---
+++ src/app/resume/page.tsx
@@ -0,0 +1,7 @@
+import Wrapper from "@/components/wrapperComponent";
+
+export default function Page() {
+  return <Wrapper className="flex flex-col items-center justify-center min-h-screen text-3xl font-bold">
+    File will be uploaded here soon.
+  </Wrapper>;
+}
