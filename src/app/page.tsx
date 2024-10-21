65426d40dd5762d5ca9a530c1d66881a81b813d0
Robin K <dev.robinkurian@gmail.com>
2024-10-21 10:18:43 +0530

    Refactor: home page content, add icons from react-icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,22 @@
 import Image from "next/image";
-
+import { FaLinkedin, FaGithub, FaGlobe } from "react-icons/fa";
+import { SiNetlify } from "react-icons/si";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
-          src="/icons/robin.svg"
-          alt="Next.js logo"
+          src="/icons/portfolio.svg"
+          alt="Portfolio logo"
           width={180}
           height={38}
           priority
         />
         <ul className="list-inside list-disc text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+          <li className="mb-2">
+            Developer just started working on this website.
+          </li>
           <li className="mb-2">
             A portfolio website that showcases my projects and skills.
           </li>
@@ -28,10 +32,8 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
+            <SiNetlify 
+              aria-hidden
               width={20}
               height={20}
             />
@@ -50,48 +52,42 @@ export default function Home() {
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="hhttps://www.linkedin.com/in/developer-robin-kurian/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image
+          <FaLinkedin
             aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
             width={16}
             height={16}
           />
-          Learn
+          LinkedIn
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="https://github.com/Robin-Kurian/robin-portfolio-frontend/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image
+          <FaGithub
             aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
             width={16}
             height={16}
           />
-          Examples
+          Github
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="https://robink.netlify.app/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image
+          <FaGlobe
             aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
             width={16}
             height={16}
           />
-          Go to nextjs.org →
+          Go to Portfolio →
         </a>
       </footer>
     </div>
